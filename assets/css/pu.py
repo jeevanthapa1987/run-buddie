2c1515a19235178808a5649d634c2859ff0203a1
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2022-04-05 19:48:41 -0400

    add content to hero, replace image

--- assets/css/pu.py
+++ assets/css/pu.py
@@ -210,3 +210,470 @@ body {
     line-height: 1.3;
   }
   
+
+
+   * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    /* more on this crazy alphanumerical value in a minute! */
+    color: #39a6b2;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+  
+  /* apply styles to <header> */
+  header {
+    padding: 20px 35px;
+    background-color: #39a6b2;
+  }
+  
+  header h1 {
+    font-weight: bold;
+    font-size: 36px;
+    color: #fce138;
+    margin: 0;
+    display: inline;
+  }
+  header a {
+    text-decoration: none;
+    color: #fce138;
+  }
+  
+  header nav {
+    float: right;
+    margin: 7px 0;
+  }
+  
+  header nav ul li {
+    display: inline;
+  }
+  
+  header nav ul li a {
+    margin: 0 30px;
+    font-weight: lighter;
+    font-size: 22px;
+  }
+  
+  footer {
+    background: #fce138;
+    width: 100%;
+    padding: 40px 35px;
+  }
+  
+  footer h2 {
+    display: inline;
+    color: #024e76;
+    font-size: 30px;
+    margin: 0;
+  }
+  
+  footer div {
+    float: right;
+    line-height: 1.5;
+    text-align: right;
+  }
+  
+  footer a {
+    color: #024e76;
+  }
+  
+  section {
+    padding: 60px;
+  }
+  
+  /* Hero Style Start */
+  .hero {
+    background-image: url('https://static.fullstack-bootcamp.com/module-1/hero-bg.jpg ');
+    height: 600px;
+    background-size: cover;
+    background-position: center;
+    position: relative;
+  }
+  /* Hero Style End */
+  
+  .hero-form {
+    border: 3px solid #024e76;
+    background-color: #fce138;
+    padding: 20px;
+    width: 500px;
+    color: #024e76;
+    position: absolute;
+    bottom: 120px;
+    right: 140px;
+  }
+  
+  .hero-form h3 {
+    font-size: 24px;
+    margin: 0;
+  }
+  .hero-form p {
+    margin: 5px 0 15px 0;
+  }
+  
+  .form-input {
+    border: 1px solid #024e76;
+    display: block;
+    padding: 7px 15px;
+    font-size: 16px;
+    color: #024e76;
+    width: 100%;
+    margin-bottom: 15px;
+  }
+  
+  .hero-form label {
+    margin: 0 5px;
+  }
+  
+  .hero-form button {
+    color: #fce138;
+    background-color: #024e76;
+    border: none;
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+  
+  .intro {
+    text-align: center;
+  }
+  
+  .intro p {
+    line-height: 1.7;
+    color: #39a6b2;
+    width: 80%;
+    font-size: 20px;
+    margin: 0 auto;
+  }
+  
+  .steps {
+    text-align: center;
+    background: #fce138;
+  }
+  
+  .steps div {
+    margin-bottom: 80px;
+  }
+  
+  .steps img {
+    width: 15%;
+    margin: 10px 0;
+  }
+  
+  .steps h3 {
+    color: #024e76;
+    font-size: 46px;
+    margin-top: 10px;
+  }
+  
+  .steps p {
+    color: #39a6b2;
+    font-size: 23px;
+  }
+  
+  .steps span {
+    font-size: 38px;
+  }
+  
+  .section-title {
+    font-size: 55px;
+    color: #024e76;
+    margin-bottom: 35px;
+    padding: 0 100px 20px 100px;
+    display: inline-block;
+    border-bottom: 3px solid;
+  }
+  
+  .primary-border {
+    border-color: #fce138;
+  }
+  
+  .secondary-border {
+    border-color: #39a6b2;
+  }
+  
+  .trainers {
+    text-align: center;
+  }
+  
+  .trainer {
+    width: 900px;
+    margin: 0 auto 30px auto;
+    background: #024e76;
+    color: #fce138;
+    overflow: auto;
+  }
+  
+  .trainer img {
+    width: 35%;
+    float: left;
+  }
+  
+  .trainer-bio {
+    padding: 35px;
+    float: left;
+    width: 65%;
+  }
+  
+  .trainer-bio h3 {
+    font-size: 32px;
+    margin-bottom: 8px;
+  }
+  
+  .trainer-bio h4 {
+    font-weight: lighter;
+    font-size: 26px;
+    margin-bottom: 25px;
+  }
+  
+  .trainer-bio p {
+    font-size: 17px;
+    line-height: 1.3;
+  }
+  
+  .text-left {
+    text-align: left;
+  }
+  
+  .text-right {
+    text-align: right;
+  }
+  .section-title {
+    font-size: 55px;
+    display: inline-block;
+    padding: 0 100px 20px 100px;
+    border-bottom: 3px solid;
+    margin-bottom: 35px;
+    color: #024e76;
+  }
+  /* REACH OUT STYLES START */
+.contact {
+    text-align: center;
+    background: #024e76;
+  }
+  .contact h2 {
+    color: #fce138;
+  }
+  .contact-info iframe {
+    width: 400px;
+    height: 400px;
+  }
+  .contact-info div {
+    width: 410px;
+    display: inline-block;
+    vertical-align: top;
+    text-align: left;
+    margin: 30px 0 0 60px;
+    color: white;
+  }
+  .contact-info h3 {
+    color: #fce138;
+    font-size: 32px;
+  }
+  
+  .contact-info p, .contact-info address {
+    margin: 20px 0;
+    line-height: 1.5;
+    font-size: 20px;
+    font-style: normal;
+  }
+  
+  .contact-info a {
+    color: #fce138;
+  }
+  
+  /* REACH OUT STYLES END */  
+
+
+<!-- navigation -->
+<header>
+    <h1>
+    <a href='/'>RUN BUDDY</a>
+</h1>
+    <nav>
+<!--Unordered list element-->
+<ul>
+    <!--List item element-->
+    <li>
+    <!--Anchor element-->
+    <a href="#what-we-do">What We Do</a>
+    </li>
+    <li>
+        
+        <a href="#what-you-do">What You Do</a>
+</li>
+<li>
+ 
+<a href="#your-trainers">Your Trainers</a>
+</li>
+<li>
+
+<a href="#reach-out">Reach Out</a>
+</li>        
+
+</ul>
+    </nav>
+  </header>
+ 
+  
+  <!-- hero section -->
+<section class="hero">
+  <div class="hero-form">
+    <h3>Get Started Today!</h3>
+    <p>Fill out this form and one of our trainers will schedule a consult.</p>
+    <form>
+
+      <label for="name">Enter full name:</label>
+      <input type="text" placeholder="Your Name" name="name" id="name" class="form-input"/>
+
+      <label for="email">Enter email address:</label>
+      <input type="email" placeholder="Email Address" name="email" id="email" class="form-input"/>
+
+      <label for="phone">Enter a telephone number:</label>
+      <input type="tel" placeholder="Phone Number" name="phone" id="phone" class="form-input"/>
+
+      <p>
+        Have you worked out with a trainer before?
+        <input type="radio" name="trainer-confirm" id="trainer-yes" />
+        <label for="trainer-yes">Yes</label>
+        <input type="radio" name="trainer-confirm" id="trainer-no" />
+        <label for="trainer-no">No</label>
+      </p>
+      <p>
+        <label for="checkbox" >
+          I acknowledge that I am at least 18 years of age.
+        </label>
+        <input type="checkbox" name="age-confirm" id="checkbox" />
+      </p>
+      <button type="submit">
+        Get running!
+      </button>
+    </form>
+  </div>
+</section>
+<!-- end hero -->
+  
+  <!-- "what we do" section -->
+  <section id="what-we-do" class="intro">
+    <h2  class="section-title primary-border">What We Do</h2>
+<p>butcher selfies chambray shabby chic gentrify readymade yr Echo Park XOXO Tumblr normcore Banksy direct trade Blue Bottle 
+  chillwave you probably haven't heard of them single-origin coffee Vice fanny pack fixie Odd Future Austin fingerstache pickled
+   twee synth Wes Anderson Thundercats viral bitters flannel meggings narwhal Marfa Schlitz sustainable Intelligentsia umami deep
+    v craft .</p>   
+  </section>
+  
+  <!-- "what you do" section -->
+  <section id="what-you-do" class="steps">
+    <h2  class="section-title primary-border">What You Do</h2>
+
+    <div>
+  <!-- insert this img element -->
+  <img src="file:///C:/Users/jeeva/AppData/Local/Temp/Temp1_01-04-svgs%20(2).zip/step-1.svg" alt="" />    
+      <h3>Step 1:<span> Fill Out the Form Above.</span></h3>
+      <p>You're already here, so why not?</p>
+    </div>
+  
+    <div>
+      <img src="file:///C:/Users/jeeva/AppData/Local/Temp/Temp1_01-04-svgs%20(2).zip/step-2.svg " alt="" />      
+      <h3>Step 2: Consult with One of Our Trainers.</h3>
+      <p>Are you here to build muscle, lose weight, or just feel good?</p>
+    </div>
+  
+    <div>
+      <img src="file:///C:/Users/jeeva/AppData/Local/Temp/Temp1_01-04-svgs%20(2).zip/step-3.svg " alt="" />   
+      <h3>Step 3: Get Running.</h3>
+      <p>Hit the ground running (literally) once your trainer lays out your plan.</p>
+    </div>
+  
+    <div>
+      <img src="file:///C:/Users/jeeva/AppData/Local/Temp/Temp1_01-04-svgs%20(2).zip/step-4.svg " alt="" />   
+      <h3>Step 4: See Results.</h3>
+      <p>Bi-weekly checkins with your trainer will keep you focused</p>
+    </div>
+  </section>
+  
+  <!-- "meet the trainers" section -->
+  <section  id="your-trainers" class="trainers">
+    <h2 class="section-title primary-border">Meet The Trainers</h2>
+  <article class="trainer">
+<img src="C:\Users\jeeva\Downloads\1280x960_70228P00-XVSYL.jpg" alt="Arron Stephens in his workout clothes, ready to pump iron" />
+<div class="trainer-bio text-left">
+  <h3>Arron Stephens</h3>
+        <h4>Speed / Strength</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi neque animi quo cupiditate commodi saepe culpa sed
+          itaque velit maiores optio dolorem excepturi aperiam dolores, voluptatibus suscipit amet quis repellat!
+        </p>
+      </div>
+    </article>  
+  
+
+
+    <!-- second trainer bio -->
+<article class="trainer">
+  <img src="C:\Users\jeeva\Downloads\1280x960_70228P00-XVSYL.jpg" alt="Joanna Gill cooling off after a workout" />  
+  <div class="trainer-bio text-right">
+    <h3>Joanna Gill</h3>
+    <h4>Endurance</h4>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi neque animi quo cupiditate commodi saepe culpa sed
+      itaque velit maiores optio dolorem excepturi aperiam dolores, voluptatibus suscipit amet quis repellat!
+    </p>
+  </div>
+  
+</article>
+    
+
+<!-- third trainer bio -->
+
+<article class="trainer">
+  <img src="C:\Users\jeeva\Downloads\1280x960_70228P00-XVSYL.jpg " alt="Harry Smith wearing a headband and lifting comically small pink weights" />
+  <div class="trainer-bio text-left">
+    <h3>Harry "the Headband" Smith</h3>
+    <h4>Strength</h4>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi neque animi quo cupiditate commodi saepe culpa sed
+      itaque velit maiores optio dolorem excepturi aperiam dolores, voluptatibus suscipit amet quis repellat!
+    </p>
+  </div>
+</article>
+  </section>
+  
+  <!-- "reach out" section -->
+  <section id="reach-out" class="contact">
+    <h2 class="section-title secondary-border">Reach Out</h2>
+    <div class="contact-info">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3147.1079747227936!2d-120.42364418397035!3d37.92790791110593!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8090c49129b6ac57%3A0xb56c7eb95a2cd8bd!2sMain%20St%2C%20California%2095327!5e0!3m2!1sen!2sus!4v1616426329495!5m2!1sen!2sus" 
+      width="400" 
+      height="400" 
+      style="border:0;" 
+      allowfullscreen="" 
+      loading="lazy">
+      </iframe>
+    </div>
+    <div>
+      <h3>Run Buddy</h3>
+      <p>
+        Any questions or concerns before signing up?
+        <br />
+        Let us know and we'll be happy to talk to you!
+      </p>
+      <address>
+        55 Main Street <br/>
+        Some Town, Ca <br/>
+        12345<br />
+        P: 555.RUN.BUDZ (555.786.2839)<br/>
+        E: <a href="mailto:info@runbuddy.io">info@runbuddy.io</a>
+      </address>
+    </div>   
+  </section>
+  
+  
+  <footer>
+    <h2>❤️ Made with love by Run Buddy.</h2>
+    <div>
+      <a href="#">Read Our Privacy Policy</a><br />
+      &copy; 2019 Run Buddy, Inc.
+    </div>
+  </footer>
